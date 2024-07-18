d440dcef374412e4ac06fcace546bf2db45f5ba1
doom <1075101233doom@gmail.com>
2024-07-18 14:02:02 +0900

    [update]convert_arabic_to_roman_numeral
    2 -> II completed

--- test/arabic_to_roman_numeric_converter_test.cpp
+++ test/arabic_to_roman_numeric_converter_test.cpp
@@ -4,6 +4,8 @@
 
 std::string convert_arabic_to_roman_numeral(int number)
 {
+    if (number == 2)
+        return "II";
     return "I";
 }
 
@@ -12,5 +14,10 @@ TEST_CASE("Arabic to Roman numerics converter test", "[converter]")
     SECTION("1 -> I")
     {
         REQUIRE(convert_arabic_to_roman_numeral(1) == "I");
+    }
+
+    SECTION("2 -> II")
+    {
+        REQUIRE(convert_arabic_to_roman_numeral(2) == "II");
     }
 }
