0cb877f7df105de0d482a706c86d8286eca4275b
doom <1075101233doom@gmail.com>
2024-07-18 14:57:01 +0900

    [update]convert_arabic_to_roman_numeral
    adjust indent

--- test/arabic_to_roman_numeric_converter_test.cpp
+++ test/arabic_to_roman_numeric_converter_test.cpp
@@ -10,21 +10,22 @@ struct ArabicToRomanMapping{
 
 std::string convert_arabic_to_roman_numeral(unsigned arabic_number)
 {
-    static std::array<ArabicToRomanMapping, 13> arabic_to_roman_mappings = {
-        ArabicToRomanMapping{ 1000, "M" },
-        { 900, "CM" },
-        { 500, "D" },
-        { 400, "CD" },
-        { 100, "C" },
-        { 90, "XC" },
-        { 50, "L" },
-        { 40, "XL" },
-        { 10, "X" },
-        { 9, "IX" },
-        { 5, "V" },
-        { 4, "IV" },
-        { 1, "I" }
-    };
+    static std::array<ArabicToRomanMapping, 13> arabic_to_roman_mappings =
+                {
+                    ArabicToRomanMapping{ 1000, "M" },
+                    { 900, "CM" },
+                    { 500, "D" },
+                    { 400, "CD" },
+                    { 100, "C" },
+                    { 90, "XC" },
+                    { 50, "L" },
+                    { 40, "XL" },
+                    { 10, "X" },
+                    { 9, "IX" },
+                    { 5, "V" },
+                    { 4, "IV" },
+                    { 1, "I" }
+                };
 
     std::string roman_numeral{};
 
