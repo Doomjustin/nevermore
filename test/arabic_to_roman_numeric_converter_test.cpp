b25df8dd375cc952184f9c0a0d858fbdfdf52ba7
doom <1075101233doom@gmail.com>
2024-07-18 14:41:07 +0900

    [update]convert_arabic_to_roman_numeral
    实现增量的逻辑

--- test/arabic_to_roman_numeric_converter_test.cpp
+++ test/arabic_to_roman_numeric_converter_test.cpp
@@ -1,14 +1,30 @@
 #include <Catch2/catch_test_macros.hpp>
 
 #include <string>
+#include <array>
 
-std::string convert_arabic_to_roman_numeral(int number)
+struct ArabicToRomanMapping{
+    unsigned arabic_number;
+    std::string roman_numeral;
+};
+
+std::string convert_arabic_to_roman_numeral(unsigned arabic_number)
 {
+    static std::array<ArabicToRomanMapping, 5> arabic_to_roman_mappings = {
+        ArabicToRomanMapping{ 1000, "M" },
+        { 100, "C" },
+        { 10, "X" },
+        { 5, "V" },
+        { 1, "I" }
+    };
+
     std::string roman_numeral{};
 
-    while (number >= 1) {
-        roman_numeral += "I";
-        number -= 1;
+    for (const auto& [arabic_number_unit, roman_numeral_digit]: arabic_to_roman_mappings) {
+        while (arabic_number >= arabic_number_unit) {
+            roman_numeral += roman_numeral_digit;
+            arabic_number -= arabic_number_unit;
+        }
     }
 
     return roman_numeral;
@@ -16,18 +32,27 @@ std::string convert_arabic_to_roman_numeral(int number)
 
 TEST_CASE("Arabic to Roman numerics converter test", "[converter]")
 {
-    SECTION("1 -> I")
-    {
-        REQUIRE(convert_arabic_to_roman_numeral(1) == "I");
-    }
+    REQUIRE(convert_arabic_to_roman_numeral(1) == "I");
+    REQUIRE(convert_arabic_to_roman_numeral(2) == "II");
+    REQUIRE(convert_arabic_to_roman_numeral(3) == "III");
 
-    SECTION("2 -> II")
-    {
-        REQUIRE(convert_arabic_to_roman_numeral(2) == "II");
-    }
+    REQUIRE(convert_arabic_to_roman_numeral(5) == "V");
+    REQUIRE(convert_arabic_to_roman_numeral(6) == "VI");
+    REQUIRE(convert_arabic_to_roman_numeral(7) == "VII");
+    REQUIRE(convert_arabic_to_roman_numeral(8) == "VIII");
 
-    SECTION("3 -> III")
-    {
-        REQUIRE(convert_arabic_to_roman_numeral(3) == "III");
-    }
+    REQUIRE(convert_arabic_to_roman_numeral(10) == "X");
+    REQUIRE(convert_arabic_to_roman_numeral(20) == "XX");
+    REQUIRE(convert_arabic_to_roman_numeral(30) == "XXX");
+    REQUIRE(convert_arabic_to_roman_numeral(33) == "XXXIII");
+
+    REQUIRE(convert_arabic_to_roman_numeral(100) == "C");
+    REQUIRE(convert_arabic_to_roman_numeral(200) == "CC");
+    REQUIRE(convert_arabic_to_roman_numeral(300) == "CCC");
+    REQUIRE(convert_arabic_to_roman_numeral(333) == "CCCXXXIII");
+
+    REQUIRE(convert_arabic_to_roman_numeral(1000) == "M");
+    REQUIRE(convert_arabic_to_roman_numeral(2000) == "MM");
+    REQUIRE(convert_arabic_to_roman_numeral(3000) == "MMM");
+    REQUIRE(convert_arabic_to_roman_numeral(3333) == "MMMCCCXXXIII");
 }
